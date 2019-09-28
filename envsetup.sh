f38a93ff64f4c0e4ba1a91149faff1ff7bee3272
DNI9 <itsr9boi@gmail.com>
2019-09-28 15:02:07 +0530

    using evie

--- envsetup.sh
+++ envsetup.sh
@@ -1752,4 +1752,4 @@ check_bash_version && {
 export ANDROID_BUILD_TOP=$(gettop)
 
 
-. $ANDROID_BUILD_TOP/vendor/dot/build/envsetup.sh && . dot_scripts/Lawnchair.sh && . dot_scripts/DotHome.sh
+. $ANDROID_BUILD_TOP/vendor/dot/build/envsetup.sh
